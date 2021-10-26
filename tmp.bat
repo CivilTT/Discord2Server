46e6d70d932afccc174591a0eabde3ac8d476dbf
CivilTT <civiltt.ss@gmail.com>
2021-10-27 00:23:22 +0900

    first commit

--- tmp.bat
+++
@@ -1,9 +0,0 @@
-git init
-git config --global user.name CivilTT
-git config --global user.email civiltt.ss@gmail.com
-git add -A
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/CivilTT/Discord2Server.git
-git push -u origin main
-pause
